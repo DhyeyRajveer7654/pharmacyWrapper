5a325168d15345a1e82f9b5ae0afa9b5da4115bc
DhyeyRajveer7654 <dhyeydhyey7654@gmail.com>
2025-04-11 23:28:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -865,7 +865,7 @@ elif st.session_state.current_page == 'about':
         change_page('contact')
 
 # Regulatory Page
-    elif st.session_state.current_page == 'regulatory':
+    if st.session_state.current_page == 'regulatory':
         st.markdown('<h2 class="section-title">Regulatory Compliance Services</h2>', unsafe_allow_html=True)
         st.markdown("<h3 style='color: #1e40af;'>QRx- An AI powered Reuglatory Compliance</h3>", unsafe_allow_html=True)
         
@@ -900,11 +900,11 @@ elif st.session_state.current_page == 'about':
                     st.success("Thank you for your request! Our regulatory team will contact you within 24 hours to discuss your regulatory gap analysis.")
         st.stop()
 # Quality Page
-elif st.session_state.current_page == 'quality':
-    st.markdown('<h2 class="section-title">Quality Assurance Services</h2>', unsafe_allow_html=True)
+    elif st.session_state.current_page == 'quality':
+        st.markdown('<h2 class="section-title">Quality Assurance Services</h2>', unsafe_allow_html=True)
     
     # Introduction
-    st.markdown("""
+        st.markdown("""
     <style>
         /* Global Styles */
         body {
