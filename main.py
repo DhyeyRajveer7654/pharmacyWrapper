1c2f4c685ee6af160c51e077f46c09e0765c6d71
DhyeyRajveer7654 <dhyeydhyey7654@gmail.com>
2025-04-13 00:36:26 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -497,13 +497,13 @@ st.markdown("""
 
     .main-header h1 {
         color: #000000;
-        font-size: 2rem;
+        font-size: 1.5rem;
         margin-bottom: 0.5rem;
     }
 
     .main-header p {
         color: #000000;
-        font-size: 1.1rem;
+        font-size: 1.5rem;
     }
 
     /* Results styling */
