aa6e8803a657ce7de615dc8a0a43ed25f2867330
DhyeyRajveer7654 <dhyeydhyey7654@gmail.com>
2025-04-12 13:08:19 +0530

    Add files via upload

--- prompts.py
+++ prompts.py
@@ -119,8 +119,9 @@ def getPromptForOptions(options):
     print(final_prompt)
     return final_prompt
 def getPromptForOptions(options):
-    report_type = ""
-    if options['report_type'] == "Pathway":
+      if options['report_type'] == "Pathway":
       prompt_template = PATHYWAY_PROMPT
-    final_prompt = prompt_template.substitute(product_type=options['product_type'], report_type=options['report_type'], regulatory_authorities=options['regulatory'])
-    return final_prompt
+      final_prompt = prompt_template.substitute(product_type=options['product_type'], report_type=options['report_type'], regulatory_authorities=options['regulatory'])
+      return final_prompt
+
+    
