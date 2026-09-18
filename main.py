9e58bd795df042da227ff8ffed558e36f9de075e
DhyeyRajveer7654 <dhyeydhyey7654@gmail.com>
2026-09-18 14:25:27 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -217,7 +217,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-st.markdown('<div style="background-color: white; padding: 1rem 0; border-bottom: 1px solid #e0e0e0; margin-bottom: 1rem;">', unsafe_allow_html=True)
+st.markdown
 col1, col2, col3, col4, col5, col6 = st.columns(6)
 with col1:
     st.image("qrxai.png", width=200)
